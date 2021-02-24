00a76055653d50c2f3a07d6c65a142fdd9558127
Andrey Kuksa <andrey20011005@gmail.com>
2021-02-24 15:21:59 +0300

    code fixes

--- src/test/java/by/fpmi/osi/second/entities/ConcurrentNonBlockedListTest.java
+++ src/test/java/by/fpmi/osi/second/entities/ConcurrentNonBlockedListTest.java
@@ -9,16 +9,15 @@ import java.util.stream.IntStream;
 
 public class ConcurrentNonBlockedListTest {
 
-
     @Test
     public void testAdd() throws InterruptedException {
         int expectedSize = 1000;
-        ConcurrentNonBlockedList<Integer> list = new ConcurrentNonBlockedList<>();
+        ConcurrentNonBlockedList<Integer> concurrentList = new ConcurrentNonBlockedList<>();
         Integer[] expectedElementsSorted = new Integer[expectedSize];
         Thread[] threadList = new Thread[expectedSize];
 
         IntStream.range(0, expectedSize).forEach(i -> {
-            Thread testThread = new Thread(() -> list.add(i));
+            Thread testThread = new Thread(() -> concurrentList.add(i));
             threadList[i] = testThread;
             threadList[i].start();
             expectedElementsSorted[i] = i;
@@ -28,7 +27,7 @@ public class ConcurrentNonBlockedListTest {
             threadList[i].join();
         }
 
-        Object[] elementsActual = list.toArray();
+        Object[] elementsActual = concurrentList.toArray();
         Arrays.sort(elementsActual);
         Assert.assertArrayEquals(elementsActual, expectedElementsSorted);
     }
@@ -36,36 +35,39 @@ public class ConcurrentNonBlockedListTest {
     @Test
     public void testRemove() throws InterruptedException {
         int startSize = 1000;
-        ConcurrentNonBlockedList<Integer> list = new ConcurrentNonBlockedList<>();
+        ConcurrentNonBlockedList<Integer> concurrentList = new ConcurrentNonBlockedList<>();
         for (int i = 0; i < startSize; i++) {
-            list.add(i);
+            concurrentList.add(i);
         }
+
         Thread[] threadList = new Thread[startSize];
+
         for (int i = 0; i < startSize - 1; i++) {
-            Thread testThread = new Thread(() -> list.remove(0));
+            Thread testThread = new Thread(() -> concurrentList.remove(0));
             threadList[i] = testThread;
             threadList[i].start();
         }
+
         for (int i = 0; i < startSize - 1; i++) {
             threadList[i].join();
         }
         int expectedSize = 1;
-        Assert.assertEquals(expectedSize, list.size());
+        Assert.assertEquals(expectedSize, concurrentList.size());
     }
 
     @Test
     public void testSize() throws InterruptedException {
         int expectedSize = 100;
-        ConcurrentNonBlockedList<Integer> list = new ConcurrentNonBlockedList<>();
+        ConcurrentNonBlockedList<Integer> concurrentList = new ConcurrentNonBlockedList<>();
         Thread[] threadList = new Thread[100];
         for (int i = 0; i < expectedSize; i++) {
-            Thread testThread = new Thread(() -> list.add(1));
+            Thread testThread = new Thread(() -> concurrentList.add(1));
             threadList[i] = testThread;
             threadList[i].start();
         }
         for (int i = 0; i < expectedSize; i++) {
             threadList[i].join();
         }
-        Assert.assertEquals(list.size(), expectedSize);
+        Assert.assertEquals(concurrentList.size(), expectedSize);
     }
 }
